e972b7a76b272036d14fbd28e4f61a3ff7f4c932
vearne <asdwoshiaotian@gmail.com>
2018-10-24 10:13:38 +0800

    update eclat.py

--- eclat/eclat.py
+++ eclat/eclat.py
@@ -4,19 +4,24 @@ import copy
 # item -> trans
 data = {}
 # 支持度
+# min_support = 0.2
 min_support = 0.5
+# 置信度
+min_confidence = 0.6
+
 size = 0
-result = set()
+# 结果集
+freq_itemsets = []
 
 
 def default_judge(prefix_list):
-    global result
+    global freq_itemsets
     if len(prefix_list) == 0:
         return True
 
     key = "".join(prefix_list)
     if len(data[key]) >= size * min_support:
-        result.add(key)
+        freq_itemsets.append(key)
         return True
     else:
         # print key, False
@@ -54,11 +59,61 @@ def main(file_path):
                     data[item] = set()
                 data[item].add(int(item_list[0]))
 
+    print "---1. 寻找频繁项集------"
     combination([], data.keys())
     print "len(data)", len(data)
     # 结果数据集
-    print result
+    print "频繁项集", freq_itemsets
+    print "---2. 寻找关联规则------"
+    extract_rule(freq_itemsets)
+
+
+
+def extract_rule(freq_itemsets):
+    length = len(freq_itemsets)
+    x = 0
+    y = 0
+    while x < length:
+        y = x + 1
+        while y < length:
+            jude_rule(freq_itemsets[x], freq_itemsets[y])
+            y += 1
+        x += 1
+
+def jude_rule(x, xy):
+    # print '-' * 20
+    # 判断 是否  x -> y 和判断    x -> xy 可以认为是等价的
+    global size
+    if len(x) > len(xy):
+        x, xy = xy, x
+
+    # A -> AB
+    # B -> AB
+    # 只有itemset2 包含 itemset1 才有可能有关联规则
+    if set(x).issubset(set(xy)):
+
+        # 计算置信度
+        # print 'len(data[itemset1])', len(data[x])
+        # print 'len(data[itemset2])', len(data[xy])
+
+        suport_xy = len(data[xy]) / float(size)
+        suport_x = len(data[x]) / float(size)
+        confidence = suport_xy / suport_x
+
+        y = list(set(xy) - set(x))
+        y = "".join(sorted(y))
+        suport_y = len(data[y]) / float(size)
+        # 计算提升度
+        lift = suport_xy / (suport_x * suport_y)
+        # print 'suport_xy', suport_xy, 'suport_x', suport_x, 'suport_y', suport_y
+        print x, "-->", y, "confidence", confidence, "lift", lift
+        # 提升度必须大于1, 规则才是正相关的
+        if confidence > min_confidence and lift > 1:
+            print "满足提升度要求的", "x", "-->", y, "confidence", confidence, "lift", lift
+
 
 if __name__ == "__main__":
     f = "./eclat.dat"
+    # f = "./eclat2.dat"
     main(f)
+
